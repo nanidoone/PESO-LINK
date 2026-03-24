2c5738cb8298d8fccff2628bebb3cf5236999fed
emerita <luntian429.21@gmail.com>
2026-03-24 11:44:35 +0800

    changed the title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>PESO Job Portal</title>
+        <title>Link Job Resource Portal</title>
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
